060430cbfe3c1d172e0f8f34eb397222fe05cc54
luis-rveiga <lveiga05@gmail.com>
2023-05-19 21:59:38 -0300

    exemplos de python: print e variaveis

---
+++ programacao-orientada-objetos/variables.py
@@ -0,0 +1,12 @@
+# declaring variables
+var_string = "Hello"
+var_integer = 1
+var_float = 3.14159
+var_boolean = True
+
+#printing the value of the variable
+print(var_string)
+
+# identifying the type of the variable
+print(type(var_string))
+
