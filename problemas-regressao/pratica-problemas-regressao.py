84039338f3b46b9d804295efa83e9f739105595a
luis-rveiga <lveiga05@gmail.com>
2023-05-30 20:45:04 -0300

    alteracao da pratica-problemas-regressao

--- problemas-regressao/pratica-problemas-regressao.py
+++ problemas-regressao/pratica-problemas-regressao.py
@@ -1,5 +1,4 @@
 # configuração para não exibir warnings
-
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -24,3 +23,61 @@ dataset = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
 dataset['target'] = diabetes.target
 
 print(dataset.head())
+
+# preparaão dos dados
+
+# separação em bases de treino e teste (holdout)
+array = dataset.values
+x = array[:, 0:10]  # atributos
+y = array[:, 10]  # classe (target)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=7)
+
+# criando os folds para a validação cruzada
+num_particoes = 10  # número de folds da validação cruzada
+kfold = KFold(n_splits=num_particoes, shuffle=True, random_state=7)
+
+# modelagem
+
+# definindo uma seed global para esta célula de código
+np.random.seed(7)
+
+# listas para armazenar os modelos, os resultados e os nomes dos modelos
+models = []
+results = []
+names = []
+
+models.append(('LR', LinearRegression()))
+models.append(('Ridge', Ridge()))
+models.append(('Lasso', Lasso()))
+models.append(('KNN', KNeighborsRegressor()))
+models.append(('CART', DecisionTreeRegressor()))
+models.append(('SVM', SVR()))
+
+# avaliando um modelo por vez
+for name, model in models:
+    cv_results = cross_val_score(model, x_train, y_train, cv=kfold, scoring='neg_mean_squared_error')
+    results.append(cv_results)
+    names.append(name)
+    # imprime MSE, desvio padrão  do MSE e RMSE dos 10 resultados da validação cruzada
+    msg = "%s: MSE %0.2f (%0.2f) - RMSE %0.2f" % (
+        name, abs(cv_results.mean()), cv_results.std(), np.sqrt(abs(cv_results.mean())))
+    print(msg)
+
+# boxplot de comparação dos modelos
+fig = plt.figure()
+fig.suptitle('Comparação do MSE dos Modelos')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+# criando um modelo com todo o conjunto de treino
+model = LinearRegression()
+model.fit(x_train, y_train)
+
+predictions = model.predict(x_test)
+
+mse = mean_squared_error(y_test, predictions)
+print("MSE %0.2f" % mse)
+print("RMSE %0.2f" % np.sqrt(abs(mse)))
