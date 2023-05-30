7a8ad9ce1fb49bd2b918d53ea100576005e5ec73
luis-rveiga <lveiga05@gmail.com>
2023-05-30 03:31:25 -0300

    inclusao da pratica-problemas-regressao

---
+++ problemas-regressao/pratica-problemas-regressao.py
@@ -0,0 +1,20 @@
+# configuração para não exibir warnings
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# imports necessários
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
