86aa29cd7fda866b9fa8d39fd122c493c0c4c33f
luis-rveiga <lveiga05@gmail.com>
2023-06-01 20:53:18 -0300

    complemento do codigo

--- recursos-avancados-aprendizado-supervisionado/pratica-recursos-avancados-aprendizado-supervisionado.py
+++ recursos-avancados-aprendizado-supervisionado/pratica-recursos-avancados-aprendizado-supervisionado.py
@@ -41,9 +41,9 @@ seed = 7 # semente aleatoria
 
 # separacao em conjuntos de treino e teste
 array = dataset.values
-x = array[:,0:8]
+X = array[:,0:8]
 y = array[:,8]
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, shuffle=True, random_state=seed, stratify=y) # holdout com estratificacao
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=True, random_state=seed, stratify=y) # holdout com estratificacao
 
 # parâmetros e partições da validação cruzada
 scoring = 'accuracy'
@@ -90,7 +90,7 @@ names = []
 
 # avaliação dos modelos
 for name, model in models:
-    cv_results = cross_val_score(model, x_train, y_train, cv=kfold, scoring=scoring)
+    cv_results = cross_val_score(model, X_train, y_train, cv=kfold, scoring=scoring)
     results.append(cv_results)
     names.append(name)
     msg = "%s: %f (%f)" % (name, cv_results.mean(),cv_results.std())
@@ -134,4 +134,126 @@ min_max_scaler = ('MinMaxScaler', MinMaxScaler())
 
 # dataset original
 pipelines.append(('LR-orig', Pipeline([reg_log])))
-pipelines.append(())
+pipelines.append(('KNN-orig', Pipeline([knn])))
+pipelines.append(('CART-orig', Pipeline([cart])))
+pipelines.append(('NB-orig', Pipeline([naive_bayes])))
+pipelines.append(('SVM-orig', Pipeline([svm])))
+pipelines.append(('Bag-orig', Pipeline([bagging])))
+pipelines.append(('RF-orig', Pipeline([random_forest])))
+pipelines.append(('ET-orig', Pipeline([extra_trees])))
+pipelines.append(('Ada-orig', Pipeline([adaboost])))
+pipelines.append(('GB-orig', Pipeline([gradient_boosting])))
+pipelines.append(('Vot-orig', Pipeline([voting])))
+
+# dataset padronizado
+pipelines.append(('LR-padr', Pipeline([standard_scaler, reg_log])))
+pipelines.append(('KNN-padr', Pipeline([standard_scaler, knn])))
+pipelines.append(('CART-padr', Pipeline([standard_scaler, cart])))
+pipelines.append(('NB-padr', Pipeline([standard_scaler, naive_bayes])))
+pipelines.append(('SVM-padr', Pipeline([standard_scaler, svm])))
+pipelines.append(('Bag-padr', Pipeline([standard_scaler, bagging])))
+pipelines.append(('RF-padr', Pipeline([standard_scaler, random_forest])))
+pipelines.append(('ET-padr', Pipeline([standard_scaler, extra_trees])))
+pipelines.append(('Ada-padr', Pipeline([standard_scaler, adaboost])))
+pipelines.append(('GB-padr', Pipeline([standard_scaler, gradient_boosting])))
+pipelines.append(('Vot-padr', Pipeline([standard_scaler, voting])))
+
+# dataset normalizado
+pipelines.append(('LR-norm', Pipeline([min_max_scaler, reg_log])))
+pipelines.append(('KNN-norm', Pipeline([min_max_scaler, knn])))
+pipelines.append(('CART-norm', Pipeline([min_max_scaler, cart])))
+pipelines.append(('NB-norm', Pipeline([min_max_scaler, naive_bayes])))
+pipelines.append(('SVM-norm', Pipeline([min_max_scaler, svm])))
+pipelines.append(('Bag-norm', Pipeline([min_max_scaler, bagging])))
+pipelines.append(('RF-norm', Pipeline([min_max_scaler, random_forest])))
+pipelines.append(('ET-norm', Pipeline([min_max_scaler, extra_trees])))
+pipelines.append(('Ada-norm', Pipeline([min_max_scaler, adaboost])))
+pipelines.append(('GB-norm', Pipeline([min_max_scaler, gradient_boosting])))
+pipelines.append(('Vot-norm', Pipeline([min_max_scaler, voting])))
+
+# executando os pipelines
+for name, model in pipelines:
+    cv_results = cross_val_score(model, X_train, y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %.3f (%.3f)" % (name, cv_results.mean(), cv_results.std()) # formatando para 3 casas decimais
+    print(msg)
+
+# boxplot de comparação dos modelos
+fig = plt.figure(figsize=(25,6))
+fig.suptitle('Comparação dos Modelos - Dataset original, padronizado e normalizado')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names, rotation=90)
+plt.show()
+
+# tuning do KNN
+
+np.random.seed(7) # definindo uma semente global para este bloco
+
+pipelines = []
+
+# definindo os componentes do pipeline
+knn = ('KNN', KNeighborsClassifier())
+standard_scaler = ('StandardScaler', StandardScaler())
+min_max_scaler = ('MinMaxScaler', MinMaxScaler())
+
+pipelines.append(('knn-orig', Pipeline(steps=[knn])))
+pipelines.append(('knn-padr', Pipeline(steps=[standard_scaler, knn])))
+pipelines.append(('knn-norm', Pipeline(steps=[min_max_scaler, knn])))
+
+param_grid = {
+    'KNN__n_neighbors': [1,3,5,7,9,11,13,15,17,19,21],
+    'KNN__metric': ["euclidean", "manhattan", "minkowski"],
+}
+
+# prepara e executa o GridSearchCV
+for name, model in pipelines:
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+    grid.fit(X_train, y_train)
+    # imprime a melhor configuração
+    print("Sem tratamento de missings: %s - Melhor: %f usando %s" % (name, grid.best_score_, grid.best_params_))
+
+# avaliação do modelo com o conjunto de testes
+
+# preparação do modelo
+scaler = StandardScaler().fit(X_train) # ajuste do scaler como conjunto de treino
+rescaledX = scaler.transform(X_train) # aplicação da padronização no conjunto de treino
+model = LogisticRegression(max_iter=200)
+model.fit(rescaledX, y_train)
+
+# estimativa de acurácia no conjunto de teste
+rescaledTestX = scaler.transform(X_test) # aplicação da padronização no conjunto de teste
+predictions = model.predict(rescaledTestX)
+print(accuracy_score(y_test, predictions))
+
+# preparação do modelo com TODO o dataset
+scaler = StandardScaler().fit(X) # ajuste do scaler com TODO o dataset
+rescaledX = scaler.transform(X) # aplicação da padronização com TODO o dataset
+model.fit(rescaledX, y)
+
+# novos dados - não sabemos a classe!
+data = {
+    'preg': [1, 9, 5],
+    'plas': [90, 100, 110],
+    'pres': [50, 60, 50],
+    'skin': [30, 30, 30],
+    'test': [100, 100, 100],
+    'mass': [20.0, 30.0, 40.0],
+    'pedi': [1.0, 2.0, 1.0],
+    'age': [15, 40, 40],
+    }
+
+atributos = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age']
+entrada = pd.DataFrame(data, columns=atributos)
+
+array_entrada = entrada.values
+X_entrada = array_entrada[:,0:8].astype(float)
+
+# padronização nos dados de entrada usando o scaler utilizado em X
+rescaledEntradaX = scaler.transform(X_entrada)
+print(rescaledEntradaX)
+
+# predição de classes dos dados de entrada
+saidas = model.predict(rescaledEntradaX)
+print(saidas)
