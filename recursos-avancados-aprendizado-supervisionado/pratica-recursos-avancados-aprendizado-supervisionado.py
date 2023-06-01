b228d761cacd7518a89a2b28a60c0739ff1c97ee
luis-rveiga <lveiga05@gmail.com>
2023-06-01 02:04:02 -0300

    commit inicial

--- recursos-avancados-aprendizado-supervisionado/pratica-recursos-avancados-aprendizado-supervisionado.py
+++ recursos-avancados-aprendizado-supervisionado/pratica-recursos-avancados-aprendizado-supervisionado.py
@@ -35,3 +35,103 @@ dataset = pd.read_csv(url, delimiter=',')
 
 # mostra as primeiras linhas do dataset
 print(dataset.head())
+
+test_size = 0.20 # tamanho do conjunto de teste
+seed = 7 # semente aleatoria
+
+# separacao em conjuntos de treino e teste
+array = dataset.values
+x = array[:,0:8]
+y = array[:,8]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, shuffle=True, random_state=seed, stratify=y) # holdout com estratificacao
+
+# parâmetros e partições da validação cruzada
+scoring = 'accuracy'
+num_particoes = 10
+kfold = StratifiedKFold(n_splits=num_particoes, shuffle=True, random_state=seed) # validação cruzada com estratificação
+
+np.random.seed(7) # definido uma semente global
+
+# lista que armazenará os dados
+models = []
+
+# criando os modelos e adicionando-os na lista de modelos
+models.append(('LR', LogisticRegression(max_iter=200)))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+# definindo os parâmetros do classificador base para o BaggingClassifier
+base = DecisionTreeClassifier()
+num_trees = 100
+max_features = 3
+
+# criando os modelos para o VottingClassifier
+bases = []
+model1 = LogisticRegression(max_iter=200)
+bases.append(('logistic', model1))
+model2 = DecisionTreeClassifier()
+bases.append(('cart', model2))
+model3 = SVC()
+bases.append(('svm', model3))
+
+# criando os ensembles e adicionado-os na lista de modelos
+models.append(('Bagging', BaggingClassifier(base_estimator=base, n_estimators=num_trees)))
+models.append(('RF', RandomForestClassifier(n_estimators=num_trees, max_features=max_features)))
+models.append(('ET', ExtraTreesClassifier(n_estimators=num_trees, max_features=max_features)))
+models.append(('Ada', AdaBoostClassifier(n_estimators=num_trees)))
+models.append(('GB', GradientBoostingClassifier(n_estimators=num_trees)))
+models.append(('Voting', VotingClassifier(bases)))
+
+# listas para armazenar os resultados
+results = []
+names = []
+
+# avaliação dos modelos
+for name, model in models:
+    cv_results = cross_val_score(model, x_train, y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(),cv_results.std())
+    print(msg)
+
+# boxplot de comparação dos modelos
+fig = plt.figure(figsize=(15,10))
+fig.suptitle('Comparação dos Modelos')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+np.random.seed(7) # definindo uma semente global para este bloco
+
+# listas para armazenar os pipelines e os resultados para todas as visões do dataset
+pipelines = []
+results = []
+names = []
+
+# criando os elementos do pipeline
+
+# algoritmos que serão utilizados
+reg_log = ('LR', LogisticRegression(max_iter=200))
+knn = ('KNN', KNeighborsClassifier())
+cart = ('CART', DecisionTreeClassifier())
+naive_bayes = ('NB', GaussianNB())
+svm = ('SVM', SVC())
+bagging = ('Bag', BaggingClassifier(base_estimator=base, n_estimators=num_trees))
+random_forest = ('RF', RandomForestClassifier(n_estimators=num_trees, max_features=max_features))
+extra_trees = ('ET', ExtraTreesClassifier(n_estimators=num_trees, max_features=max_features))
+adaboost = ('Ada', AdaBoostClassifier(n_estimators=num_trees))
+gradient_boosting = ('GB', GradientBoostingClassifier(n_estimators=num_trees))
+voting = ('Voting', VotingClassifier(bases))
+
+# transformações que serão utilizadas
+standard_scaler = ('StandardScaler', StandardScaler())
+min_max_scaler = ('MinMaxScaler', MinMaxScaler())
+
+# montando os pipelines
+
+# dataset original
+pipelines.append(('LR-orig', Pipeline([reg_log])))
+pipelines.append(())
